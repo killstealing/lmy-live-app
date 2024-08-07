88b6c145027ebaa6e54d85f15b18ab9bc3dade74
killstealing <13840935238@163.com>
2024-08-07 20:32:10 +0800

    add  12-10   到12-15 PK功能

--- lmy-live-gift-provider/src/main/java/org/lmy/live/gift/provider/consumer/SendGiftConsumer.java
+++ lmy-live-gift-provider/src/main/java/org/lmy/live/gift/provider/consumer/SendGiftConsumer.java
@@ -29,7 +29,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -44,11 +46,21 @@ import java.util.stream.Collectors;
 @Configuration
 public class SendGiftConsumer implements InitializingBean {
 
-    private static final Long MAX_PK_NUM=1000l;
-    private static final Long MIN_PK_NUM=0l;
-
-
     private static final Logger LOGGER = LoggerFactory.getLogger(SendGiftConsumer.class);
+
+    private static final Long PK_INIT_NUM = 50L;
+    private static final Long PK_MAX_NUM = 100L;
+    private static final Long PK_MIN_NUM = 0L;
+    private String LUA_SCRIPT =
+            "if (redis.call('exists', KEYS[1])) == 1 then " +
+                    " local currentNum=redis.call('get',KEYS[1]) " +
+                    " if (tonumber(currentNum)<=tonumber(ARGV[2]) and tonumber(currentNum)>=tonumber(ARGV[3])) then " +
+                    " return redis.call('incrby',KEYS[1],tonumber(ARGV[4])) " +
+                    " else return currentNum end " +
+                    "else " +
+                    "redis.call('set', KEYS[1], tonumber(ARGV[1])) " +
+                    "redis.call('EXPIRE', KEYS[1], 3600 * 12) " +
+                    "return ARGV[1] end";
 
     @Resource
     private RocketMQConsumerProperties rocketMQConsumerProperties;
@@ -57,11 +69,11 @@ public class SendGiftConsumer implements InitializingBean {
     @Resource
     private GiftProviderCacheKeyBuilder cacheKeyBuilder;
     @DubboReference
-    private ILmyCurrencyAccountRpc accountRpc;
+    private ILmyCurrencyAccountRpc qiyuCurrencyAccountRpc;
     @DubboReference
     private ILivingRoomRpc livingRoomRpc;
     @DubboReference
-    private ImRouterRpc imRouterRpc;
+    private ImRouterRpc routerRpc;
 
     @Override
     public void afterPropertiesSet() throws Exception {
@@ -70,7 +82,6 @@ public class SendGiftConsumer implements InitializingBean {
         mqPushConsumer.setVipChannelEnabled(false);
         mqPushConsumer.setNamesrvAddr(rocketMQConsumerProperties.getNameSrv());
         mqPushConsumer.setConsumerGroup(rocketMQConsumerProperties.getGroupName() + "_" + SendGiftConsumer.class.getSimpleName());
-        //一次从broker中拉取10条消息到本地内存当中进行消费
         mqPushConsumer.setConsumeMessageBatchMaxSize(10);
         mqPushConsumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
         //监听礼物缓存数据更新的行为
@@ -78,93 +89,127 @@ public class SendGiftConsumer implements InitializingBean {
         mqPushConsumer.setMessageListener((MessageListenerConcurrently) (msgs, context) -> {
             for (MessageExt msg : msgs) {
                 SendGiftMq sendGiftMq = JSON.parseObject(new String(msg.getBody()), SendGiftMq.class);
-                String cacheKey = cacheKeyBuilder.buildGiftConsumeCacheKey(sendGiftMq.getUuid());
-                boolean lockStatus = redisTemplate.opsForValue().setIfAbsent(cacheKey, -1, 5, TimeUnit.MINUTES);
-                if(!lockStatus){
+                String mqConsumerKey = cacheKeyBuilder.buildGiftConsumeCacheKey(sendGiftMq.getUuid());
+                boolean lockStatus = redisTemplate.opsForValue().setIfAbsent(mqConsumerKey, -1, 5, TimeUnit.MINUTES);
+                if (!lockStatus) {
                     //代表曾经消费过
                     continue;
                 }
                 AccountTradeReqDTO tradeReqDTO = new AccountTradeReqDTO();
                 tradeReqDTO.setUserId(sendGiftMq.getUserId());
                 tradeReqDTO.setNum(sendGiftMq.getPrice());
-                Integer type = sendGiftMq.getType();
-                AccountTradeRespDTO accountTradeRespDTO = accountRpc.consumeForSendGift(tradeReqDTO);
-                JSONObject jsonObject=new JSONObject();
+                AccountTradeRespDTO tradeRespDTO = qiyuCurrencyAccountRpc.consumeForSendGift(tradeReqDTO);
                 //如果余额扣减成功
-                if (accountTradeRespDTO.isSuccess()) {
-                    if (type== SendGiftTypeEnum.DEFAULT_SEND_GIFT.getCode()){
-                        //todo 出发礼物特效推送功能
-                        jsonObject.put("url",sendGiftMq.getUrl());
-                        sendGiftSingleton(sendGiftMq.getReceiverId(), ImMsgBizCodeEnum.LIVING_ROOM_SEND_GIFT_SUCCESS.getCode(),jsonObject);
-                    }else{
-                        //pk类型的送礼 要通知什么给直播间的用户
-                        //url 礼物特效全直播间可见
-                        //todo 进度条全直播间可见
-                        // 1000,进度条长度一共是1000，每个礼物对于进度条的影响就是一个数值（500（A）：500（B），550：450）
-                        // 直播pk进度是不是以roomId为维度，string，送礼（A）incr，送礼给（B）就是decr。
-                        Integer roomId = sendGiftMq.getRoomId();
-                        LivingRoomRespDTO livingRoomRespDTO = livingRoomRpc.queryByRoomId(roomId);
-                        Long objUserId=livingRoomRpc.queryOnlinePkUserId(roomId);
-                        if(objUserId==null||livingRoomRespDTO==null||livingRoomRespDTO.getAnchorId()==null){
-                            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
-                        }
-                        Long pkUserId=livingRoomRespDTO.getAnchorId();
-                        Long pkNum=0l;
-                        String pkNumCacheKey = cacheKeyBuilder.buildPKNumCacheKey(roomId);
-                        String pkNumSeqCacheKey = cacheKeyBuilder.buildPKNumSeqCacheKey(roomId);
-                        Long pkNumSeqVal = redisTemplate.opsForValue().increment(pkNumSeqCacheKey);
-                        if(sendGiftMq.getReceiverId().equals(pkUserId)){
-                            // increase ,   500/500,   一个礼物50， 550/450
-                            pkNum = redisTemplate.opsForValue().increment(pkNumCacheKey, sendGiftMq.getPrice());
-                            if(pkNum>=MAX_PK_NUM){
-                                jsonObject.put("winnerId", pkUserId);
-                                pkNum=MAX_PK_NUM;
-                            }
-                        }else if(sendGiftMq.getUserId().equals(objUserId)){
-                            pkNum= redisTemplate.opsForValue().decrement(pkNumCacheKey, sendGiftMq.getPrice());
-                            if(pkNum<=MIN_PK_NUM){
-                                jsonObject.put("winnerId", objUserId);
-                                pkNum=MIN_PK_NUM;
-                            }
-                        }
-                        jsonObject.put("sendGiftSeqNum",pkNumSeqVal);
-                        jsonObject.put("pkNum",pkNum);
-                        LivingRoomReqDTO livingRoomReqDTO=new LivingRoomReqDTO();
-                        livingRoomReqDTO.setRoomId(sendGiftMq.getRoomId());
-                        List<Long> userIdList = livingRoomRpc.queryUserIdByRoomId(livingRoomReqDTO);
-                        jsonObject.put("url",sendGiftMq.getUrl());
-                        batchSendGift(userIdList,ImMsgBizCodeEnum.LIVING_ROOM_PK_SEND_GIFT_SUCCESS.getCode(), jsonObject);
+                Integer sendGiftType = sendGiftMq.getType();
+                JSONObject jsonObject = new JSONObject();
+                //改成全直播间可见
+                if (tradeRespDTO.isSuccess()) {
+                    Long receiverId = sendGiftMq.getReceiverId();
+                    if (SendGiftTypeEnum.DEFAULT_SEND_GIFT.getCode().equals(sendGiftType)) {
+                        //触发礼物特效推送功能
+                        jsonObject.put("url", sendGiftMq.getUrl());
+                        LivingRoomReqDTO reqDTO = new LivingRoomReqDTO();
+                        reqDTO.setAppId(AppIdEnum.LMY_LIVE_BIZ.getCode());
+                        reqDTO.setRoomId(sendGiftMq.getRoomId());
+                        List<Long> userIdList = livingRoomRpc.queryUserIdByRoomId(reqDTO);
+                        this.batchSendImMsg(userIdList, ImMsgBizCodeEnum.LIVING_ROOM_SEND_GIFT_SUCCESS, jsonObject);
+                    } else if (SendGiftTypeEnum.PK_SEND_GIFT.getCode().equals(sendGiftType)) {
+                        this.pkImMsgSend(jsonObject, sendGiftMq, receiverId);
                     }
                 } else {
-                    //todo 利用im将发送失败的消息告知用户
-                    jsonObject.put("msg",accountTradeRespDTO.getMsg());
-                    sendGiftSingleton(sendGiftMq.getUserId(), ImMsgBizCodeEnum.LIVING_ROOM_SEND_GIFT_FAIL.getCode(),jsonObject);
+                    //利用im将发送失败的消息告知用户
+                    jsonObject.put("msg", tradeRespDTO.getMsg());
+                    this.sendImMsgSingleton(sendGiftMq.getUserId(), ImMsgBizCodeEnum.LIVING_ROOM_SEND_GIFT_FAIL.getCode(), jsonObject);
                 }
-                LOGGER.info("[SendGiftConsumer] msg is {}", msg);
+//                LOGGER.info("[SendGiftConsumer] msg is {}", msg);
             }
             return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
         });
         mqPushConsumer.start();
-        LOGGER.info("mq消费者:SendGiftConsumer启动成功,namesrv is {}", rocketMQConsumerProperties.getNameSrv());
+        LOGGER.info("mq消费者启动成功,namesrv is {}", rocketMQConsumerProperties.getNameSrv());
     }
 
-    private void sendGiftSingleton(Long userId,int bizCode,JSONObject jsonObject){
-        ImMsgBodyDTO imMsgBody=new ImMsgBodyDTO();
+
+    /**
+     * 单独发送im消息
+     *
+     * @param userId
+     * @param bizCode
+     * @param jsonObject
+     */
+    private void sendImMsgSingleton(Long userId, int bizCode, JSONObject jsonObject) {
+        ImMsgBodyDTO imMsgBody = new ImMsgBodyDTO();
         imMsgBody.setAppId(AppIdEnum.LMY_LIVE_BIZ.getCode());
         imMsgBody.setBizCode(bizCode);
         imMsgBody.setUserId(userId);
-        imMsgBody.setData(JSON.toJSONString(jsonObject));
-        imRouterRpc.sendMsg(imMsgBody);
+        imMsgBody.setData(jsonObject.toJSONString());
+        routerRpc.sendMsg(imMsgBody);
     }
-    private void batchSendGift(List<Long> userIdList,int bizCode,JSONObject jsonObject){
-        List<ImMsgBodyDTO> imMsgBodyDTOList = userIdList.stream().map(userId -> {
+
+    private void pkImMsgSend(JSONObject jsonObject, SendGiftMq sendGiftMq, Long receiverId) {
+        //pk类型的送礼 要通知什么给直播间的用户
+        //url 礼物特效全直播间可见
+        //todo 进度条全直播间可见
+        // 1000,进度条长度一共是1000，每个礼物对于进度条的影响就是一个数值（500（A）：500（B），550：450）
+        // 直播pk进度是不是以roomId为维度，string，送礼（A）incr，送礼给（B）就是decr。
+        Integer roomId = sendGiftMq.getRoomId();
+        String isOverCacheKey = cacheKeyBuilder.buildLivingPkIsOver(roomId);
+        if (redisTemplate.hasKey(isOverCacheKey)) {
+            return;
+        }
+        LivingRoomRespDTO respDTO = livingRoomRpc.queryByRoomId(roomId);
+        Long pkObjId = livingRoomRpc.queryOnlinePkUserId(roomId);
+        if (pkObjId == null || respDTO == null || respDTO.getAnchorId() == null) {
+            return;
+        }
+        Long pkUserId = respDTO.getAnchorId();
+        Long pkNum = 0L;
+        String pkNumKey = cacheKeyBuilder.buildPKNumCacheKey(roomId);
+        DefaultRedisScript<Long> redisScript = new DefaultRedisScript();
+        redisScript.setScriptText(LUA_SCRIPT);
+        redisScript.setResultType(Long.class);
+        Long sendGiftSeqNum = System.currentTimeMillis();
+        if (pkUserId.equals(receiverId)) {
+            Integer moveStep = sendGiftMq.getPrice() / 10;
+            pkNum = this.redisTemplate.execute(redisScript, Collections.singletonList(pkNumKey), PK_INIT_NUM, PK_MAX_NUM, PK_MIN_NUM, moveStep);
+            if (PK_MAX_NUM <= pkNum) {
+                jsonObject.put("winnerId", pkUserId);
+            }
+        } else if (pkObjId.equals(receiverId)) {
+            Integer moveStep = sendGiftMq.getPrice() / 10 * -1;
+            pkNum = this.redisTemplate.execute(redisScript, Collections.singletonList(pkNumKey), PK_INIT_NUM, PK_MAX_NUM, PK_MIN_NUM, moveStep);
+            if (PK_MIN_NUM >= pkNum) {
+                this.redisTemplate.opsForValue().set(cacheKeyBuilder.buildLivingPkIsOver(roomId),-1);
+                jsonObject.put("winnerId", pkObjId);
+            }
+        }
+        jsonObject.put("receiverId", sendGiftMq.getReceiverId());
+        jsonObject.put("sendGiftSeqNum", sendGiftSeqNum);
+        jsonObject.put("pkNum", pkNum);
+        jsonObject.put("url", sendGiftMq.getUrl());
+        LivingRoomReqDTO livingRoomReqDTO = new LivingRoomReqDTO();
+        livingRoomReqDTO.setRoomId(roomId);
+        livingRoomReqDTO.setAppId(AppIdEnum.LMY_LIVE_BIZ.getCode());
+        List<Long> userIdList = livingRoomRpc.queryUserIdByRoomId(livingRoomReqDTO);
+        this.batchSendImMsg(userIdList, ImMsgBizCodeEnum.LIVING_ROOM_PK_SEND_GIFT_SUCCESS, jsonObject);
+    }
+
+    /**
+     * 批量发送im消息
+     *
+     * @param userIdList
+     * @param imMsgBizCodeEnum
+     * @param jsonObject
+     */
+    private void batchSendImMsg(List<Long> userIdList, ImMsgBizCodeEnum imMsgBizCodeEnum, JSONObject jsonObject) {
+        List<ImMsgBodyDTO> imMsgBodies = userIdList.stream().map(userId -> {
             ImMsgBodyDTO imMsgBody = new ImMsgBodyDTO();
             imMsgBody.setAppId(AppIdEnum.LMY_LIVE_BIZ.getCode());
-            imMsgBody.setBizCode(bizCode);
+            imMsgBody.setBizCode(imMsgBizCodeEnum.getCode());
             imMsgBody.setUserId(userId);
-            imMsgBody.setData(JSON.toJSONString(jsonObject));
+            imMsgBody.setData(jsonObject.toJSONString());
             return imMsgBody;
         }).collect(Collectors.toList());
-        imRouterRpc.batchSendMsg(imMsgBodyDTOList);
+        routerRpc.batchSendMsg(imMsgBodies);
     }
 }
